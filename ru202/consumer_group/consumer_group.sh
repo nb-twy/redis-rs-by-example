3e18c4c2d5b405584ec8a28909256e7f37ffd812
Kurt Schoener <kjschoener@gmail.com>
2022-01-10 05:23:34 -0700

    [RRBE-42] Add build & run script

---
+++ ru202/consumer_group/consumer_group.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This is a lab, so everything is built with the debug profile.
+
+# If the main directory exists, build consumer_group_main
+if [[ -d ./main ]]; then
+    echo "[>] Building consumer_group_main..."
+    cd main
+    cargo build
+    cd ..
+    echo -e "\n\n"
+fi
+
+# If the consumer directory exists, build consumer_group_consumer
+if [[ -d ./consumer ]]; then
+    echo "[>] Building consumer_group_consumer..."
+    cd consumer
+    cargo build
+    cd ..
+    echo -e "\n\n"
+fi
+
+# If the bin directory does not exist, create it.
+if [[ ! -d ./bin ]]; then
+    mkdir bin
+fi
+
+# If the bin directory exists
+# and if the executables exist,
+# copy the executables into bin.
+MAIN="./main/target/debug/consumer_group_main"
+CONSUMER="./consumer/target/debug/consumer_group_consumer"
+
+if [[ -f "$MAIN" ]]; then
+    cp -v "$MAIN" ./bin/
+fi
+
+if [[ -f "$CONSUMER" ]]; then
+    cp -v "$CONSUMER" ./bin/
+fi
+
+# Run the lab by executing consumer_group_main
+echo -e "\n\n#### Consumer Group Lab ###\n\n"
+cd bin
+./consumer_group_main
